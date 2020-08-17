d7b4556bcba857c4b89833aab7204ed98dcee8e2
tasfia tasnim oroni <tasfia.oroni@mds.torrens.edu.au>
2020-08-18 01:30:13 +1000

    moving cars but need to setup the collision

---
+++ Assets/CarSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject car;
+    public float spawnDelay = 1f;
+    float nextTimeToSpawn = 0f;
+   // float countDownTimer = 3f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(nextTimeToSpawn<= Time.time/10)
+        {
+            SpawnCar();
+            nextTimeToSpawn = (Time.time/10) + spawnDelay;
+        }
+    }
+    void SpawnCar ()
+    {
+        Instantiate(car);
+    }
+}
+ 
--- Assets/Scenes/Vehicle.cs
+++ Assets/Scenes/Vehicle.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Security.Cryptography;
 using UnityEngine;
 
 /// <summary>
@@ -10,8 +12,12 @@ public class Vehicle : MonoBehaviour
     /// <summary>
     /// -1 = left, 1 = right
     /// </summary>
+    public Rigidbody2D rb;
     public int moveDirection = 0; //this variable is used to indicate the direction of the veichle is moving in.
-    public float speed;//This variable is used to control the seed of the vehicle.
+    //public float speed;//This variable is used to control the seed of the vehicle.
+    public float minSpeed = 8f;
+    public float maxSpeed = 12f;
+    float speed = 1f;
     public Vector2 startingPosition;//This variable is to be used to indicate where on the map the vehicle starts(or spawn).
     public Vector2 endPosition;//This variable is to be used to indicate  the final destination of the vehicle.
 
@@ -19,12 +25,13 @@ public class Vehicle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(minSpeed, maxSpeed);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
     }
 }
